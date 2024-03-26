bc1d8d5e52add6bfdefb4c7b290491ff1556281f
Nnaemeka Okonkwo <nokonkwo8426@gmail.com>
2024-03-25 20:42:21 -0400

    Enhanced Microservice on K8s

--- src/main/java/dev/nokonkwo/socials/list/SocialMediaController.java
+++ src/main/java/dev/nokonkwo/socials/list/SocialMediaController.java
@@ -4,6 +4,8 @@ package dev.nokonkwo.socials.list;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,6 +22,20 @@ public class SocialMediaController {
     @GetMapping("/accounts")
     public List<SocialMediaAccounts> getSocialMediaAccounts() {
         return socialMediaProperties.getAccounts();
+    }
+    @GetMapping("/config")
+    public String envVar(){
+        Map<String, String> map = System.getenv();
+        StringBuilder sb  = new StringBuilder();
+
+        for(String key : map.keySet()){
+            sb.append(key);
+            sb.append(" = ");
+            sb.append(map.get(key));
+            sb.append("\n");
+        }
+        return sb.toString();
+
     }
 
     @PostMapping("/accounts/add")
@@ -41,5 +57,21 @@ public class SocialMediaController {
         if (id >= 0 && id < accounts.size()) {
             accounts.remove(id);
         }
+    }
+
+    @RequestMapping(value = "/fib", method = RequestMethod.GET)
+    public String generateFibonacci(@RequestParam("length") int length) {
+        List<Integer> fibonacci = new ArrayList<>();
+
+        int num1 = 0;
+        int num2 = 1;
+
+        for(int i = 0; i < length; i++){
+            fibonacci.add(num1);
+            int num3 = num1 + num2;
+            num1 = num2;
+            num2 = num3;
+        }
+        return fibonacci.toString();
     }
 }
